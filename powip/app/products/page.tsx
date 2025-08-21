d27e0a63ea15e792ae98fc969d7a286e7491cf42
MauricioTognoli <tognolimauricio@gmail.com>
2025-08-21 19:34:44 -0300

    frontend prueba

--- powip/app/products/page.tsx
+++ powip/app/products/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Ventas() {
+export default function Productos() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-8">Ventas</h1>
+      <h1 className="text-4xl font-bold text-blue-600 mb-8">Productos</h1>
       <Link href="/" className="text-blue-500 hover:underline text-xl">
         Volver a Home
       </Link>
