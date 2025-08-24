cdfd76bf797c978b3629cc5806ab72ca6e4166e1
Mauricio Tognoli <138159666+MauricioTognoli@users.noreply.github.com>
2025-08-23 21:47:06 -0300

    Variables de colores en globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-6xl font-bold text-blue-600 mb-8">Home</h1>
+      <h1 className="text-6xl font-bold text-green mb-8">Home</h1>
       <div className="flex direction-col space-x-6">
         <Link href="/ventas" className="text-blue-500 hover:underline text-xl">
           Ventas
--- app/pedidos/page.tsx
+++ app/pedidos/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Pedidos() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-8">Pedidos</h1>
+      <h1 className="text-4xl font-bold text-green mb-8">Pedidos</h1>
       <Link href="/" className="text-blue-500 hover:underline text-xl">
         Volver a Home
       </Link>
