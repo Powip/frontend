9bc7f390265e2c8b2eee43f67f64d0448cb69c1e
OctavioToledo <octatoledo7@gmail.com>
2025-11-11 22:46:19 -0300

    Comment 'auth' in dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,13 +8,13 @@ import { useEffect } from "react";
 export default function DashboardPage() {
   const { auth, logout } = useAuth();
   const router = useRouter();
-
+/* 
   useEffect(() => {
     if (!auth) router.push("/login");
   }, [auth]);
 
   if (!auth) return null;
-
+ */
   return (
     <div className="min-h-screen">
       <Stats />
