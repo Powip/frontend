2e030a26ffa3aa0dfbaf70f658e74aa6321b8f5e
martinezmauri <maurimartine01@gmail.com>
2025-12-20 14:16:20 -0300

    boton de copiar seleccionado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,21 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function Home() {
   const router = useRouter();
+  const { auth, loading } = useAuth();
 
   useEffect(() => {
-    router.replace("/dashboard");
-  }, []);
+    if (loading) return; // ⏳ esperamos rehidratación
+
+    if (auth) {
+      router.replace("/dashboard");
+    } else {
+      router.replace("/login");
+    }
+  }, [auth, loading, router]);
 
   return null;
 }
